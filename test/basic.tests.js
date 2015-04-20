ff6cc2e27a9b0bd225461e89a6c241a782f09f93
Rob Bearman <robbear@hyperfine.com>
2015-04-20 09:40:33 -0700

    Added simple content changed unit test

--- test/basic.tests.js
+++ test/basic.tests.js
@@ -8,15 +8,34 @@ suite('basic', function() {
     container.innerHTML = '';
   });
 
-  /* TODO
-  test('to do: tests', function(done) {
+  test('instantiation', function(done) {
     var fixture = document.createElement('basic-framed-content');
     container.appendChild(fixture);
     flush(function() {
+      assert(fixture);
       done();
     });
   });
-  */
+
+  test('content changed', function(done) {
+    var fixture = document.createElement('basic-framed-content');
+    var contentDiv = document.getElementById('contentDiv');
+    assert(contentDiv);
+    container.appendChild(fixture);
+    flush(function() {
+      fixture.innerHTML = contentDiv.innerHTML;
+      // We must use setTimeout rather than flush since the
+      // basic-framed-content code sets a timeout tick of 1 to
+      // allow the browser to re-render.
+      setTimeout(function() {
+        var computedHeight = parseFloat(window.getComputedStyle(fixture, null).getPropertyValue("height"));
+        computedHeight = Math.floor(computedHeight);
+        assert(fixture._lastHeight > 1);
+        assert.equal(fixture._lastHeight, computedHeight);
+        done();
+      }, 10);
+    });
+  });
 
 });
 
